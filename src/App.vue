02b1a464e3c932ff26fe4b3965cb82166ad53cde
Diany Passos <dianytnpassos@gmail.com>
2024-12-05 18:52:39 -0300

    atualizando a programação do coração

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
 //função, imports, variáveis
 import coracao from "./assets/coracao.png";
 import curtida from "./assets/curtida.png";
+import {ref} from "vue";
+
+const imagens = ref([
+  {src: coracao, isLiked: false},
+  {src: coracao, isLiked: false},
+  {src: coracao, isLiked: false}
+]);
+
+function like(index) {
+  imagens.value[index].isLiked = !imagens.value[index].isLiked;
+  imagens.value[index].src = imagens.value[index].isLiked ? curtida : coracao;
+}
+
 </script>
 
 <template>
@@ -16,7 +29,7 @@ import curtida from "./assets/curtida.png";
         <p>17 de ago, 2024</p>
         <!-- diretivas -->
         <!-- para renderizar um dado da lógica (tudo que estiver detro do script), se for em uma tag (ex. tag p) usaremos as {{  }}. Como src, href é um atributo usaremos o v-bind, : é o atalho do v-bind -->
-        <img :src="coracao" alt="coração curtido">
+        <img :src="imagens[0].src" alt="coração para dar 'curtida' na publicação" @click="like(0)">
       </div>
       <h3>O que é linguagem de programação? Conheça as principais</h3>
       <p class="texto">Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
@@ -24,7 +37,7 @@ import curtida from "./assets/curtida.png";
     <section>
       <div>
         <p>12 de jul, 2024</p>
-        <img :src="coracao" alt="">
+        <img :src="imagens[1].src" alt="coração para dar 'curtida' na publicação" @click="like(1)">
       </div>
       <h3>GitHub agora permite fazer login sem precisar de senha</h3>
       <p class="texto">O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.</p>
@@ -32,7 +45,7 @@ import curtida from "./assets/curtida.png";
     <section>
       <div>
         <p>21 de jun, 2024</p>
-        <img :src="coracao" alt="">
+        <img :src="imagens[2].src" alt="coração para dar 'curtida' na publicação" @click="like(2)">
       </div>
       <h3>Por que os hiperlinks são azuis em sua maioria?</h3>
       <p class="texto">Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks - uma palavra ou termo clicável que direciona o leitor.</p>
@@ -107,6 +120,7 @@ div{
   font-weight: 500;
   color: #E07B67;
   font-size: 0.8rem;
+  cursor: pointer;
 }
 
 h3{
