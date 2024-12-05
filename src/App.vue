b0c795fc13fc2f9261e8f0f742eaf95ec72dbdf8
Diany Passos <dianytnpassos@gmail.com>
2024-12-05 01:09:56 -0300

    ajeitando o style da página

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import curtida from "./assets/curtida.png";
 <template>
   <header>
     <h1><span>CodeLab</span>Vue</h1>
-    <input type="text">
+    <input type="text" placeholder="Pesquisar no blog">
   </header>
   <main>
     <section>
@@ -19,7 +19,7 @@ import curtida from "./assets/curtida.png";
         <img :src="coracao" alt="coração curtido">
       </div>
       <h3>O que é linguagem de programação? Conheça as principais</h3>
-      <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
+      <p class="texto">Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
     </section>
     <section>
       <div>
@@ -27,7 +27,7 @@ import curtida from "./assets/curtida.png";
         <img :src="coracao" alt="">
       </div>
       <h3>GitHub agora permite fazer login sem precisar de senha</h3>
-      <p>O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.</p>
+      <p class="texto">O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.</p>
     </section>
     <section>
       <div>
@@ -35,7 +35,7 @@ import curtida from "./assets/curtida.png";
         <img :src="coracao" alt="">
       </div>
       <h3>Por que os hiperlinks são azuis em sua maioria?</h3>
-      <p>Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks - uma palavra ou termo clicável que direciona o leitor.</p>
+      <p class="texto">Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks - uma palavra ou termo clicável que direciona o leitor.</p>
     </section>
   </main>
   <!--tags, ou seja tudo que será renderizado na tela-->
@@ -52,10 +52,13 @@ header{
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  font-family: "Space Grotesk", sans-serif;
+  font-weight: 500;
 
   input{
     width: 40vw;
-    height: 3vh;
+    height: 4vh;
+    padding: 8px 8px;
     border-radius: 8px;
     background-color: #202024;
     border: 2px solid #252529;
@@ -72,4 +75,51 @@ header{
 
 };
 
+main{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #121214;
+  height: 80vh;
+}
+
+section{
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  background-color: #202024;
+  margin: 1% 0%;
+  padding: 2% 2%;
+  border-radius: 8px;
+  border: 2px solid #252529;
+  transition: all 0.3s ease;
+
+  &:hover{
+    border: 2px solid #E07B67;
+  }
+}
+
+div{
+  display: flex;
+  justify-content: space-between;
+  font-family: "Inter", serif;
+  font-weight: 500;
+  color: #E07B67;
+  font-size: 0.8rem;
+}
+
+h3{
+  font-family: "Space Grotesk", serif;
+  color: #FFFFFF;
+  margin-bottom: 1%;
+  font-weight: 500;
+}
+
+.texto{
+  font-family: "Inter", serif;
+  color: #AFABB6;
+  font-weight: 500;
+  font-size: 0.9rem;
+}
 </style>
